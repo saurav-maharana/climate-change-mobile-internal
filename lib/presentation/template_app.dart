72f38799db35bc6c1b4ead818dfa61c9f51f336a
Saurav Maharana <saurav@wednesday.is>
2022-06-02 16:34:20 +0530

    added custom Material App

--- lib/presentation/template_app.dart
+++ lib/presentation/template_app.dart
@@ -1,15 +1,12 @@
-import 'package:dynamic_color/dynamic_color.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/flavors/flavor.dart';
 import 'package:flutter_template/flavors/flavor_config.dart';
 import 'package:flutter_template/navigation/base/app_router.dart';
-import 'package:flutter_template/presentation/base/theme/theme_data/template_app_theme_data.dart';
-import 'package:flutter_template/presentation/base/widgets/snackbar/snackbar.dart';
-import 'package:flutter_template/presentation/base/widgets/theme/theme_listener.dart';
+import 'package:flutter_template/presentation/destinations/openweather/main_home.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TemplateApp extends StatelessWidget {
   TemplateApp({Key? key}) : super(key: key);
@@ -24,33 +21,41 @@ class TemplateApp extends StatelessWidget {
         kDebugMode &&
         FlavorConfig.instance.flavor == Flavor.dev;
 
+    // return ProviderScope(
+    //   child: ThemeStateListener(
+    //     builder: (themeState) => DynamicColorBuilder(
+    //       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+    //         final lightTheme = (themeState.isDynamic && lightDynamic != null)
+    //             ? buildTheme(lightDynamic.harmonized())
+    //             : material3LightTheme;
+    //         final darkTheme = (themeState.isDynamic && darkDynamic != null)
+    //             ? buildTheme(darkDynamic.harmonized())
+    //             : material3DarkTheme;
+    //         return MaterialApp.router(
+    //           useInheritedMediaQuery: useDevicePreview,
+    //           theme: lightTheme,
+    //           darkTheme: darkTheme,
+    //           themeMode: themeState.themeMode,
+    //           routerDelegate: _appRouter.delegate(
+    //             initialRoutes: [
+    //               const OpenWeatherHomeScreen(),
+    //             ],
+    //           ),
+    //           routeInformationParser: _appRouter.defaultRouteParser(),
+    //           localizationsDelegates: context.localizationDelegates,
+    //           supportedLocales: context.supportedLocales,
+    //           locale: context.locale,
+    //           scaffoldMessengerKey: scaffoldMessengerKey,
+    //         );
+    //       },
+    //     ),
+    //   ),
+    // );
     return ProviderScope(
-      child: ThemeStateListener(
-        builder: (themeState) => DynamicColorBuilder(
-          builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-            final lightTheme = (themeState.isDynamic && lightDynamic != null)
-                ? buildTheme(lightDynamic.harmonized())
-                : material3LightTheme;
-            final darkTheme = (themeState.isDynamic && darkDynamic != null)
-                ? buildTheme(darkDynamic.harmonized())
-                : material3DarkTheme;
-            return MaterialApp.router(
-              useInheritedMediaQuery: useDevicePreview,
-              theme: lightTheme,
-              darkTheme: darkTheme,
-              themeMode: themeState.themeMode,
-              routerDelegate: _appRouter.delegate(
-                initialRoutes: [
-                  const OpenWeatherSearchRouteBody(),
-                ],
-              ),
-              routeInformationParser: _appRouter.defaultRouteParser(),
-              localizationsDelegates: context.localizationDelegates,
-              supportedLocales: context.supportedLocales,
-              locale: context.locale,
-              scaffoldMessengerKey: scaffoldMessengerKey,
-            );
-          },
+      child: MaterialApp(
+        home: OpenWeatherHome(),
+        theme: ThemeData(
+          fontFamily: GoogleFonts.dmSans().fontFamily,
         ),
       ),
     );
