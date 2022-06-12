6e72853b513d804a8ab46f67b4e45baa3c3c0aa3
Saurav Maharana <saurav@wednesday.is>
2022-06-12 19:13:03 +0530

    rolled back to previous state

--- lib/presentation/destinations/openweather/home/home/home_page.dart
+++ lib/presentation/destinations/openweather/home/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/presentation/base/page/base_page.dart';
-import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_intent.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_state.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model.dart';
 import 'package:flutter_template/presentation/entity/screen/screen.dart';
@@ -12,20 +11,9 @@ class OWHome extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return BasePage<OpenWeatherHomeScreen, OpenWeatherHomeScreenState,
-            OpenWeatherViewModel>(
-        viewModelProvider: openWeatherHomeViewModel,
-        body: const Text("Something"),
-        appBarActions: () => [
-              IconButton(
-                onPressed: () {
-                  final viewModel =
-                      ref.watch(openWeatherHomeViewModel.notifier);
-                  viewModel.onIntent(
-                    OpenWeatherHomeIntent.search(searchTerm: 'Pune'),
-                  );
-                },
-                icon: Icon(Icons.info),
-              ),
-            ]);
+        OpenWeatherViewModel>(
+      viewModelProvider: openWeatherHomeViewModel,
+      body: const Text("Something"),
+    );
   }
 }
