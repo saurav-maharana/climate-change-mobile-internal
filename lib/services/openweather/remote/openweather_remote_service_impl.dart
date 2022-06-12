dcd6120b63547acd74f247311d025144874dc771
Saurav Maharana <saurav@wednesday.is>
2022-06-12 19:10:07 +0530

    removed unwanted lOGS

--- lib/services/openweather/remote/openweather_remote_service_impl.dart
+++ lib/services/openweather/remote/openweather_remote_service_impl.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_template/services/entity/openWeather/remote_current_weather_openweather.dart';
 import 'package:flutter_template/services/openweather/remote/openweather_remote_service.dart';
-import 'package:flutter_template/foundation/extensions/object_ext.dart';
 
 class OpenWeatherRemoteServiceImpl implements OpenWeatherRemoteService {
   final Dio dio;
@@ -11,11 +10,11 @@ class OpenWeatherRemoteServiceImpl implements OpenWeatherRemoteService {
   });
 
   @override
-  Future<CurrentWeatherOpenWeather> searchCity({required String cityName}) async {
+  Future<CurrentWeatherOpenWeather> searchCity(
+      {required String cityName}) async {
     final response = await dio.get(
       'data/2.5/weather?q=$cityName&units=metric',
     );
-    logD("Response from Service :: $response");
 
     return CurrentWeatherOpenWeather.fromJson(response.data);
   }
