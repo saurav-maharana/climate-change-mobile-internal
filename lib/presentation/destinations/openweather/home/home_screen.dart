96dc87505a72b2e3927c184f0ede6329b7e6a1b0
Saurav Maharana <saurav@wednesday.is>
2022-06-13 00:08:31 +0530

    added consumer widget, fixed structuring of the code

--- lib/presentation/destinations/openweather/home/home_screen.dart
+++ lib/presentation/destinations/openweather/home/home_screen.dart
@@ -1,65 +1,53 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template/presentation/destinations/openweather/forecast/forecast.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_intent.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model.dart';
+import 'package:flutter_template/presentation/destinations/openweather/pollution/pollution_info.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class OpenWeatherHome extends ConsumerWidget {
-  const OpenWeatherHome({Key? key}) : super(key: key);
+  OpenWeatherHome({Key? key}) : super(key: key);
 
-//   const OpenWeatherHome({Key? key}) : super(key: key);
+  late List<bool> isSelectedList;
+  late List<bool> isSelectedListLanguage;
+  final TextEditingController _controller = TextEditingController();
+  String? cityName;
 
-//   @override
-//   State<OpenWeatherHome> createState() => _OpenWeatherHomeState();
-// }
-
-// class _OpenWeatherHomeState extends State<OpenWeatherHome> {
-//   late List<bool> isSelectedList;
-//   late List<bool> isSelectedListLanguage;
-//   final TextEditingController _controller = TextEditingController();
-//   String? cityName;
-
-//   Future<void> showOptionsMenu(int hiveIndex) async {
-//     int? selected = await showMenu(
-//       position: const RelativeRect.fromLTRB(100, 00, 30, 30),
-//       context: context,
-//       items: [
-//         const PopupMenuItem(
-//           value: 0,
-//           child: Text("Home"),
-//         ),
-//         const PopupMenuItem(
-//           value: 1,
-//           child: Text("Pollution Info"),
-//         ),
-//         const PopupMenuItem(
-//           child: Text("Forecast"),
-//         ),
-//       ],
-//     );
-//     if (selected == 0) {
-//       Navigator.of(context).pushAndRemoveUntil(
-//           MaterialPageRoute(builder: (context) => const OpenWeatherHome()),
-//           (route) => false);
-//     } else if (selected == 1) {
-//       Navigator.of(context).pushAndRemoveUntil(
-//           MaterialPageRoute(builder: (context) => const PollutionScreen()),
-//           (route) => false);
-//     } else if (selected == 3) {
-//       Navigator.of(context).pushAndRemoveUntil(
-//           MaterialPageRoute(builder: (context) => const WeatherForecast()),
-//           (route) => false);
-//     } else {}
-//   }
-
-//   @override
-//   void initState() {
-//     isSelectedList = [true, false];
-//     isSelectedListLanguage = [true, false];
-//     super.initState();
-//   }
+  Future<void> showOptionsMenu(BuildContext context, int hiveIndex) async {
+    int? selected = await showMenu(
+      position: const RelativeRect.fromLTRB(100, 00, 30, 30),
+      context: context,
+      items: [
+        const PopupMenuItem(
+          value: 0,
+          child: Text("Home"),
+        ),
+        const PopupMenuItem(
+          value: 1,
+          child: Text("Pollution Info"),
+        ),
+        const PopupMenuItem(
+          child: Text("Forecast"),
+        ),
+      ],
+    );
+    if (selected == 0) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => OpenWeatherHome()),
+          (route) => false);
+    } else if (selected == 1) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const PollutionScreen()),
+          (route) => false);
+    } else if (selected == 3) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const WeatherForecast()),
+          (route) => false);
+    } else {}
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -127,7 +115,7 @@ class OpenWeatherHome extends ConsumerWidget {
                     Padding(
                       padding: const EdgeInsets.only(left: 10.0, right: 10.0),
                       child: TextField(
-                        // controller: _controller,
+                        controller: _controller,
                         decoration: InputDecoration(
                           suffixIcon: IconButton(
                             onPressed: () async {
