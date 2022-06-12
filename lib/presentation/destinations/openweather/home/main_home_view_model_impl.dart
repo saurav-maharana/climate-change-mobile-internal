eea1011896e7e156342db7624ec4c2a0441a8c29
Saurav Maharana <saurav@wednesday.is>
2022-06-12 12:37:10 +0530

    created viewModel IMPL for Home Page

---
+++ lib/presentation/destinations/openweather/home/main_home_view_model_impl.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_intent.dart';
+import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_state.dart';
+import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model.dart';
+import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
+import '../../../../interactor/openweather/openweather_interactor.dart';
+
+class OpenWeatherViewModelImpl extends OpenWeatherViewModel {
+  final OpenWeatherInteractor openWeatherInteractor;
+
+  OpenWeatherViewModelImpl({
+    required this.openWeatherInteractor,
+  }) : super(_initialState);
+
+  @override
+  onInit() {
+    final result = openWeatherInteractor.search('Pune');
+    print(result);
+  }
+
+  static OpenWeatherHomeScreenState get _initialState =>
+      OpenWeatherHomeScreenState(
+          currentWeatherValue: List.empty(),
+          toolbar: UIToolbar(
+            title: "",
+            hasBackButton: false,
+          ),
+          showLoading: false);
+
+  @override
+  void onIntent(OpenWeatherHomeIntent intent) {
+    intent.when(search: (cityName) {
+      openWeatherInteractor.search(cityName);
+    });
+  }
+}
