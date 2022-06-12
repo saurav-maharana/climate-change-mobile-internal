845171677c77d9375f886d25ab63f0ee3f097943
Saurav Maharana <saurav@wednesday.is>
2022-06-12 12:38:13 +0530

    Intent for Home page

---
+++ lib/presentation/destinations/openweather/home/home_screen_intent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_template/presentation/entity/intent/intent.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_screen_intent.freezed.dart';
+
+@freezed
+class OpenWeatherHomeIntent with _$OpenWeatherHomeIntent implements BaseIntent {
+  factory OpenWeatherHomeIntent.search({required String searchTerm}) =
+  _OpenWeatherHomeIntent_Search;
+}
