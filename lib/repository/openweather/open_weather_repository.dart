9d58d2b38e3dca4c5df2a067b1ff9f35de5467b4
Saurav Maharana <saurav@wednesday.is>
2022-06-12 12:33:18 +0530

    added domain mapper to Repository layer

--- lib/repository/openweather/open_weather_repository.dart
+++ lib/repository/openweather/open_weather_repository.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter_template/domain/entity/openweather/current_weather.dart';
 
 abstract class OpenWeatherRepository {
--- lib/repository/openweather/open_weather_repository_impl.dart
+++ lib/repository/openweather/open_weather_repository_impl.dart
@@ -2,12 +2,15 @@ import 'package:flutter_template/domain/entity/openweather/current_weather.dart'
 import 'package:flutter_template/foundation/extensions/object_ext.dart';
 import 'package:flutter_template/repository/openweather/open_weather_repository.dart';
 import 'package:flutter_template/services/openweather/remote/openweather_remote_service.dart';
+import 'package:flutter_template/repository/openweather/domain_openweather_mapper.dart';
 
 class OpenWeatherRepositoryImpl implements OpenWeatherRepository {
   final OpenWeatherRemoteService openWeatherRemoteService;
+  final DomainOpenWeatherMapper domainOpenWeatherMapper;
 
   OpenWeatherRepositoryImpl({
     required this.openWeatherRemoteService,
+    required this.domainOpenWeatherMapper,
   });
 
   @override
@@ -15,6 +18,8 @@ class OpenWeatherRepositoryImpl implements OpenWeatherRepository {
     final results =
         await openWeatherRemoteService.searchCity(cityName: searchCityName);
 
-    return results;
+    logD('$results');
+
+    return domainOpenWeatherMapper.map(results);
   }
 }
