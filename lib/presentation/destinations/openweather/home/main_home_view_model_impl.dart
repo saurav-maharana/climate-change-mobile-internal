a3dfa4608ede708928bab70c902943df135015aa
Saurav Maharana <saurav@wednesday.is>
2022-06-13 16:59:53 +0530

    reset VM to original state

--- lib/presentation/destinations/openweather/home/main_home_view_model_impl.dart
+++ lib/presentation/destinations/openweather/home/main_home_view_model_impl.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
+import 'package:flutter_template/domain/entity/openweather/current_weather.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_intent.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_state.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model.dart';
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
-import '../../../../domain/entity/openweather/current_weather.dart';
 import '../../../../interactor/openweather/openweather_interactor.dart';
+import 'package:flutter_template/foundation/extensions/object_ext.dart';
 
 class OpenWeatherViewModelImpl extends OpenWeatherViewModel {
   final OpenWeatherInteractor openWeatherInteractor;
@@ -15,34 +14,23 @@ class OpenWeatherViewModelImpl extends OpenWeatherViewModel {
   }) : super(_initialState);
 
   @override
-  onInit() {
-    CurrentWeather(
-        cityName: "",
-        description: "",
-        currentTemperature: 0.0,
-        feelsLike: 0.0,
-        maximumTemperature: 0.0,
-        minimumTemperature: 0.0,
-        presssure: 0.0,
-        humidity: 0.0,
-        visibility: 0.0,
-        windSpeed: 0.0);
-    openWeatherInteractor.search('');
-  }
+  onInit() {}
 
   static OpenWeatherHomeScreenState get _initialState =>
       OpenWeatherHomeScreenState(
-          currentWeatherValue: StreamController.broadcast(),
-          toolbar: UIToolbar(
-            title: "",
-            hasBackButton: false,
-          ),
-          showLoading: false);
+        toolbar: UIToolbar(
+          title: "",
+          hasBackButton: false,
+        ),
+        showLoading: false,
+      );
 
   @override
   void onIntent(OpenWeatherHomeIntent intent) {
     intent.when(search: (cityName) {
-      openWeatherInteractor.search(cityName);
+      final results = openWeatherInteractor.search(cityName);
+
+      setState((state) => state.copyWith());
     });
   }
 }
