7e5fe177cf14a6c9d2800be3df1af994a1761c84
Saurav Maharana <saurav@wednesday.is>
2022-06-13 17:31:59 +0530

    removed unused imports

--- lib/presentation/destinations/openweather/home/home_screen_state.dart
+++ lib/presentation/destinations/openweather/home/home_screen_state.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_template/domain/entity/openweather/current_weather.dart';
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
 import 'package:flutter_template/presentation/entity/screen/screen_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
