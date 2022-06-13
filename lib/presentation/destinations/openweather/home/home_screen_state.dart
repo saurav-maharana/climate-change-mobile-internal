384f5dd0e9af8d857941ee2f37ca0707e1984686
Saurav Maharana <saurav@wednesday.is>
2022-06-13 12:30:31 +0530

    replaced LIST with StreamController

--- lib/presentation/destinations/openweather/home/home_screen_state.dart
+++ lib/presentation/destinations/openweather/home/home_screen_state.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
 import 'package:flutter_template/presentation/entity/screen/screen_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -5,10 +7,12 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'home_screen_state.freezed.dart';
 
 @freezed
-class OpenWeatherHomeScreenState with _$OpenWeatherHomeScreenState implements ScreenState {
+class OpenWeatherHomeScreenState
+    with _$OpenWeatherHomeScreenState
+    implements ScreenState {
   factory OpenWeatherHomeScreenState({
-    required List currentWeatherValue,
+    required StreamController currentWeatherValue,
     required UIToolbar toolbar,
     required bool showLoading,
-}) = _OpenWeatherHomeScreenState;
+  }) = _OpenWeatherHomeScreenState;
 }
