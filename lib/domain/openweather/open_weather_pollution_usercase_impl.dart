782e2c3b575dc2e3aede7eddf05efb95a0127836
Saurav Maharana <saurav@wednesday.is>
2022-06-14 18:00:57 +0530

    Use Case implementation for Pollution Screen

---
+++ lib/domain/openweather/open_weather_pollution_usercase_impl.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_template/domain/entity/openweather/pollution_info.dart';
+import 'package:flutter_template/domain/openweather/open_weather_pollution_usecase.dart';
+import 'package:flutter_template/repository/openweather/open_weather_pollution_repository.dart';
+
+class OpenWeatherPollutionUsecaseImpl extends OpenWeatherPollutionUseCase {
+  final OpenWeatherPollutionRepository openWeatherPollutionRepository;
+
+  OpenWeatherPollutionUsecaseImpl({
+    required this.openWeatherPollutionRepository,
+  });
+
+  @override
+  Future<OpenWeatherPollutionInfo> callInternal(String param) async {
+    return await openWeatherPollutionRepository.getPollutionInfo(
+      // ignore: todo
+      "23",
+      "12",
+    ); // TODO: Remove the hardcoded values
+  }
+}
