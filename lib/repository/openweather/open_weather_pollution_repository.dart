f91e9d684ab74a6c3ad9d69a51bfd819dd821fc8
Saurav Maharana <saurav@wednesday.is>
2022-06-14 16:39:28 +0530

    removed unused imports; fixed linting

--- lib/repository/openweather/open_weather_pollution_repository.dart
+++ lib/repository/openweather/open_weather_pollution_repository.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_template/domain/entity/openweather/pollution_info.dart';
-import 'package:flutter_template/services/entity/openWeather/remote_pollution_current_weather.dart';
 
 abstract class OpenWeatherPollutionRepository {
   Future<OpenWeatherPollutionInfo> getPollutionInfo(
