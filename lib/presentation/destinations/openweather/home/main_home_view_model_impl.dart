be07f9fc2fb16308a3f9c56f1f336eeb6dad42f3
Saurav Maharana <saurav@wednesday.is>
2022-06-15 13:11:34 +0530

    modified Init method; now fetching User Current Location and showing data likewise

--- lib/presentation/destinations/openweather/home/main_home_view_model_impl.dart
+++ lib/presentation/destinations/openweather/home/main_home_view_model_impl.dart
@@ -3,17 +3,42 @@ import 'package:flutter_template/presentation/destinations/openweather/home/home
 import 'package:flutter_template/presentation/destinations/openweather/home/home_screen_state.dart';
 import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model.dart';
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import '../../../../interactor/openweather/openweather_interactor.dart';
 
 class OpenWeatherViewModelImpl extends OpenWeatherViewModel {
   final OpenWeatherInteractor openWeatherInteractor;
+  double latitude = 0.0;
+  double longitude = 0.0;
+  String cityName = "";
 
   OpenWeatherViewModelImpl({
     required this.openWeatherInteractor,
   }) : super(_initialState);
 
   @override
-  onInit() {}
+  onInit() async {
+    await GeolocatorPlatform.instance.requestPermission();
+    await GeolocatorPlatform.instance
+        .getCurrentPosition(
+            locationSettings: const LocationSettings(
+      accuracy: LocationAccuracy.high,
+    ))
+        .then((value) {
+      latitude = value.latitude;
+      longitude = value.longitude;
+    });
+
+    await GeocodingPlatform.instance
+        .placemarkFromCoordinates(latitude, longitude)
+        .then((value) {
+      cityName = value[0].subAdministrativeArea!;
+    }).then((_) async {
+      final result = await openWeatherInteractor.search(cityName);
+      setState((state) => state.copyWith(currentWeather: result));
+    });
+  }
 
   static OpenWeatherHomeScreenState get _initialState =>
       OpenWeatherHomeScreenState(
