fbed07a5fc64a7b572cbd926c17810bb84aa2f8f
Saurav Maharana <saurav@wednesday.is>
2022-06-16 14:44:00 +0530

    Weather Forecast IMPL of Interactor

---
+++ lib/interactor/openweather/openweather_forecast_interactor_impl.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_template/domain/entity/openweather/weather_forecast.dart';
+import 'package:flutter_template/domain/openweather/open_weather_forecast_usecase.dart';
+import 'package:flutter_template/interactor/openweather/openweather_forecast_interactor.dart';
+
+class OpenWeatherForecastInteractorImpl extends OpenWeatherForecastInteractor {
+  final OpenWeatherForecastUseCase openWeatherForecastUseCase;
+
+  OpenWeatherForecastInteractorImpl({
+    required this.openWeatherForecastUseCase,
+  });
+
+  @override
+  Future<OpenWeatherForecast> getForecast(
+      String latitude, String longitude) async {
+    final searchResults = await openWeatherForecastUseCase(param: latitude);
+
+    return searchResults.when(
+      success: (data) => data,
+      error: (e) => "$e" as OpenWeatherForecast,
+    );
+  }
+}
