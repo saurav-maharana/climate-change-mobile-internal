1ba1906220d737b1da0287f4a96ecf6e6689d3bf
Saurav Maharana <saurav@wednesday.is>
2022-06-16 14:44:27 +0530

    Intent for forecast screen

---
+++ lib/presentation/destinations/openweather/forecast/forecast_screen_intent.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_template/presentation/entity/intent/intent.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'forecast_screen_intent.freezed.dart';
+
+@freezed
+class OpenWeatherForecastIntent
+    with _$OpenWeatherForecastIntent
+    implements BaseIntent {
+  factory OpenWeatherForecastIntent.search(
+      {required String latitude,
+      required String longitude}) = _OpenWeatherForecastIntent_Search;
+}
