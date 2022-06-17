21770eea463969d1d15d1c159e0e7618368fba99
Saurav Maharana <saurav@wednesday.is>
2022-06-17 12:34:55 +0530

    added code to init to dispay result based on globalCityName; fixed init to show result based on custom CityName

--- lib/presentation/destinations/openweather/forecast/forecast_screen_viewmodel_impl.dart
+++ lib/presentation/destinations/openweather/forecast/forecast_screen_viewmodel_impl.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_template/domain/entity/openweather/weather_forecast.dart';
+import 'package:flutter_template/foundation/extensions/object_ext.dart';
 import 'package:flutter_template/interactor/openweather/openweather_forecast_interactor.dart';
 import 'package:flutter_template/presentation/destinations/openweather/forecast/forecast_screen_intent.dart';
 import 'package:flutter_template/presentation/destinations/openweather/forecast/forecast_screen_state.dart';
 import 'package:flutter_template/presentation/destinations/openweather/forecast/forecast_screen_viewmodel.dart';
+import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model_impl.dart';
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
+import 'package:geocoding/geocoding.dart';
 
 class OpenWeatherForecastViewModelImpl extends OpenWeatherForecastViewModel {
   OpenWeatherForecastInteractor openWeatherForecastInteractor;
+  double latitude = 0.0;
+  double longitude = 0.0;
 
   OpenWeatherForecastViewModelImpl({
     required this.openWeatherForecastInteractor,
@@ -14,18 +19,16 @@ class OpenWeatherForecastViewModelImpl extends OpenWeatherForecastViewModel {
 
   @override
   onInit() async {
-    final result = await openWeatherForecastInteractor.getForecast("12", "43");
+    await GeocodingPlatform.instance
+        .locationFromAddress(globalCityName)
+        .then((value) => {
+              latitude = value[0].latitude,
+              longitude = value[0].longitude,
+            });
+
+    final result = await openWeatherForecastInteractor.getForecast(
+        latitude.toString(), longitude.toString());
     setState((state) => state.copyWith(openWeatherForecast: result));
-  }
-
-  @override
-  void onIntent(OpenWeatherForecastIntent intent) {
-    intent.when(search: (latitude, longitude) async {
-      final results =
-          await openWeatherForecastInteractor.getForecast("54", "32");
-
-      setState((state) => state.copyWith(openWeatherForecast: results));
-    });
   }
 
   static OpenWeatherForecastScreenState get _initState =>
@@ -34,4 +37,21 @@ class OpenWeatherForecastViewModelImpl extends OpenWeatherForecastViewModel {
         toolbar: UIToolbar(title: "", hasBackButton: false),
         showLoading: false,
       );
+
+  @override
+  void onIntent(OpenWeatherForecastIntent intent) async {
+    await GeocodingPlatform.instance
+        .locationFromAddress(globalCityName)
+        .then((value) => {
+              latitude = value[0].latitude,
+              longitude = value[0].longitude,
+            });
+
+    intent.when(search: (globalCityName) async {
+      final result = await openWeatherForecastInteractor.getForecast(
+          latitude.toString(), longitude.toString());
+
+      setState((state) => state.copyWith(openWeatherForecast: result));
+    });
+  }
 }
