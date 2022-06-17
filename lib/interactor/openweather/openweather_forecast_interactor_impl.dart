4bff3d56c35c1044024057d55933c1847335e462
Saurav Maharana <saurav@wednesday.is>
2022-06-17 11:02:55 +0530

    passing the latitude and longitude in a Tuple to make API call to UseCase

--- lib/interactor/openweather/openweather_forecast_interactor_impl.dart
+++ lib/interactor/openweather/openweather_forecast_interactor_impl.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_template/domain/entity/openweather/weather_forecast.dart';
 import 'package:flutter_template/domain/openweather/open_weather_forecast_usecase.dart';
 import 'package:flutter_template/interactor/openweather/openweather_forecast_interactor.dart';
+import 'package:tuple/tuple.dart';
 
 class OpenWeatherForecastInteractorImpl extends OpenWeatherForecastInteractor {
   final OpenWeatherForecastUseCase openWeatherForecastUseCase;
@@ -12,7 +13,8 @@ class OpenWeatherForecastInteractorImpl extends OpenWeatherForecastInteractor {
   @override
   Future<OpenWeatherForecast> getForecast(
       String latitude, String longitude) async {
-    final searchResults = await openWeatherForecastUseCase(param: latitude);
+    final searchResults =
+        await openWeatherForecastUseCase(param: Tuple2(longitude, latitude));
 
     return searchResults.when(
       success: (data) => data,
--- lib/interactor/openweather/openweather_pollution_interactor_impl.dart
+++ lib/interactor/openweather/openweather_pollution_interactor_impl.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_template/domain/entity/openweather/pollution_info.dart';
 import 'package:flutter_template/domain/openweather/open_weather_pollution_usecase.dart';
 import 'package:flutter_template/interactor/openweather/openweather_pollution_interactor.dart';
+import 'package:tuple/tuple.dart';
 
 class OpenWeatherPollutionInteractorImpl
     extends OpenWeatherPollutionInteractor {
@@ -13,7 +14,8 @@ class OpenWeatherPollutionInteractorImpl
   @override
   Future<OpenWeatherPollutionInfo> getPollutionInfo(
       String latitude, String longitude) async {
-    final result = await openWeatherPollutionUseCase(param: latitude);
+    final result =
+        await openWeatherPollutionUseCase(param: Tuple2(latitude, longitude));
 
     return result.when(
       success: (data) {
