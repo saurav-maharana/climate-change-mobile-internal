8a08e146d911ba2d42c134fd90b78cf87fae6a5e
Saurav Maharana <saurav@wednesday.is>
2022-06-17 11:00:16 +0530

    Now accepting multiple parameters to make API call

--- lib/domain/openweather/open_weather_forecast_usecase.dart
+++ lib/domain/openweather/open_weather_forecast_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_template/domain/base/usecase/base_async_use_case.dart';
 import 'package:flutter_template/domain/entity/openweather/weather_forecast.dart';
+import 'package:tuple/tuple.dart';
 
 abstract class OpenWeatherForecastUseCase
-    extends BaseAsyncUseCase<String, OpenWeatherForecast> {}
+    extends BaseAsyncUseCase<Tuple2<String, String>, OpenWeatherForecast> {}
