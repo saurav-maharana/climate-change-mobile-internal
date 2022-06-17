bce7ae090644f7018bc85c4a5232c97de32c0585
Saurav Maharana <saurav@wednesday.is>
2022-06-17 12:01:15 +0530

    Segregated repetative code into one single file

---
+++ lib/presentation/destinations/openweather/forecast/widget/forecast_scroll_structure.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ForecastScrollStrcutre extends StatelessWidget {
+  final String dayOfWeek;
+  final double temperatureOnThatDay;
+
+  const ForecastScrollStrcutre({
+    Key? key,
+    required this.dayOfWeek,
+    required this.temperatureOnThatDay,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const VerticalDivider(
+          thickness: 2,
+          color: Colors.black,
+        ),
+        Text(
+          dayOfWeek,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
+        const SizedBox(
+          height: 18.0,
+        ),
+        Text(
+          temperatureOnThatDay.toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+}
