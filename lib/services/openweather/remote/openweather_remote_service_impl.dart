5a34e9c5902d881abd4eb7591ffcd31368c44e01
Saurav Maharana <saurav@wednesday.is>
2022-06-21 15:51:06 +0530

    broke one big URL into different QueryParameters

--- lib/services/openweather/remote/openweather_remote_service_impl.dart
+++ lib/services/openweather/remote/openweather_remote_service_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_template/presentation/destinations/openweather/home/main_home_view_model_impl.dart';
 import 'package:flutter_template/services/entity/openWeather/remote_current_weather_openweather.dart';
 import 'package:flutter_template/services/openweather/remote/openweather_remote_service.dart';
 
@@ -12,8 +13,15 @@ class OpenWeatherRemoteServiceImpl implements OpenWeatherRemoteService {
   @override
   Future<CurrentWeatherOpenWeather> searchCity(
       {required String cityName}) async {
+    final queryParam = {
+      'q': cityName,
+      'units': units,
+      'lang': language,
+    };
+
     final response = await dio.get(
-      'data/2.5/weather?q=$cityName&units=metric',
+      'data/2.5/weather',
+      queryParameters: queryParam,
     );
 
     return CurrentWeatherOpenWeather.fromJson(response.data);
